a392ab37f6bd18ee899cf8dcfccfb8bda7d7ff26
ISRAEL-TARAZONA\israel.tarazona_prag <itarazon@bancolombia.com.co>
2026-02-22 18:47:33 -0500

    Primer commit

--- src/app/ui/pages/pages.component.ts
+++ src/app/ui/pages/pages.component.ts
@@ -1,5 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6zf7qBkPEFvaO8mA9xq73bUI8Jbkg0Ew",
+  authDomain: "seo-pragma-practica.firebaseapp.com",
+  projectId: "seo-pragma-practica",
+  storageBucket: "seo-pragma-practica.firebasestorage.app",
+  messagingSenderId: "306032719133",
+  appId: "1:306032719133:web:45778f0f9f68c9e1f05df4",
+  measurementId: "G-03SEDGV4P7"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @Component({
   selector: 'app-pages',
@@ -8,6 +26,10 @@ import { RouterModule } from '@angular/router';
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
+
+
 export class PagesComponent {
+
+
 
 }
